84adf2e7ddf61555ef808d907d366dadd8d82d57
Florian Gudat <florian.gudat@mgm-tp.com>
2016-12-14 21:27:28 +0100

    Rearrange folder structure

--- index.js
+++ index.js
@@ -3,18 +3,17 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use('/screens', express.static(__dirname + '/screens'));
-app.use('/public', express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + '/app/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/app/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
   response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
 
