2ac641a57a7b848fe61170f27cc589147e00c05c
Florian Gudat <florian.gudat@mgm-tp.com>
2017-01-06 19:37:50 +0100

    Add windows support

--- infra/tasks/serve.js
+++ infra/tasks/serve.js
@@ -1,23 +1,27 @@
 import gulp from 'gulp';
 import browserSync from 'browser-sync';
 import dir from '../shared/directories';
+import sequence   from 'run-sequence';
+
 import middleware from '../shared/middleware';
 import mocks from '../shared/mocks';
 
-gulp.task('styles-watch', ['styles'], (done) => {
+gulp.task('reload', [], (done) => {
   browserSync.reload();
   done();
 });
 
-gulp.task('scripts-watch', ['scripts'], (done) => {
-  browserSync.reload();
-  done();
-});
+gulp.task('styles-watch', [], (done) =>
+  sequence('styles', 'reload', done)
+);
 
-gulp.task('assets-watch', ['assets'], (done) => {
-  browserSync.reload();
-  done();
-});
+gulp.task('scripts-watch', [], (done) =>
+  sequence('scripts', 'reload', done)
+);
+
+gulp.task('assets-watch', [], (done) =>
+  sequence('assets', 'reload', done)
+);
 
 gulp.task('serve', () => {
 
@@ -35,12 +39,15 @@ gulp.task('serve', () => {
   gulp.watch([
     '*.css', '**/*.css'
   ], {cwd: dir.src}, ['styles-watch']);
+
   gulp.watch([
     '*.js', '**/*.js'
   ], {cwd: dir.src}, ['scripts-watch']);
+
   gulp.watch([
     '*', '**/*',
     '!*.css', '!**/*.css',
     '!*.js', '!**/*.js'
   ], {cwd: dir.src}, ['assets-watch']);
+
 });
