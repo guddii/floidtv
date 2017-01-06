72bb56590770082c42c87cdba9f22d8510f8d902
Florian Gudat <guddii@users.noreply.github.com>
2017-01-06 21:54:08 +0100

    Add windows support (#12)

--- infra/tasks/scripts.js
+++ infra/tasks/scripts.js
@@ -1,42 +1,17 @@
-import gulp         from 'gulp';
-import babel        from 'gulp-babel';
-import browserify   from 'browserify';
-import source       from 'vinyl-source-stream';
-import buffer       from 'vinyl-buffer';
-import gutil        from 'gulp-util';
-import uglify       from 'gulp-uglify';
-import sourcemaps   from 'gulp-sourcemaps';
-
-import sequence   from 'run-sequence';
+import gulp       from 'gulp';
+import browserify from 'browserify';
+import babelify   from 'babelify';
+import source     from 'vinyl-source-stream';
+import gutil      from 'gulp-util';
 
 import dir from '../shared/directories';
 
-gulp.task('scripts-transpile', () => {
-  gulp.src([dir.src + '/*.js', dir.src + '/**/*.js'])
-    .pipe(sourcemaps.init())
-    .pipe(babel({
-      presets: ['es2015']
-    }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(dir.dest));
-});
-
-gulp.task('scripts-bundle', () => {
-  const b = browserify({
-    entries: dir.dest + '/main.js',
-    debug: false,
-  });
-
-  return b.bundle()
-    .pipe(source(dir.dest + '/main.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(uglify())
+gulp.task('scripts', function () {
+  browserify({debug: false})
+    .transform(babelify)
+    .require(dir.src + '/main.js', {entry: true})
+    .bundle()
     .on('error', gutil.log)
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./'));
+    .pipe(source('main.js'))
+    .pipe(gulp.dest(dir.dest + '/'));
 });
-
-gulp.task('scripts', (done) =>
-  sequence('scripts-transpile', 'scripts-bundle', done)
-);
