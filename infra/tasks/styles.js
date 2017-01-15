2decd3aa4b6adb594f32c5d23a96236ef78d6897
Florian Gudat <guddii@users.noreply.github.com>
2017-01-15 14:58:08 +0100

    Update styles.js

--- infra/tasks/styles.js
+++ infra/tasks/styles.js
@@ -2,6 +2,7 @@ import gulp from 'gulp';
 import postcss from 'gulp-postcss';
 import cssnext from 'postcss-cssnext';
 import sass from 'gulp-sass';
+import sourcemaps from 'gulp-sourcemaps';
 import dir from '../shared/directories';
 
 gulp.task('styles', () => {
@@ -9,11 +10,13 @@ gulp.task('styles', () => {
     cssnext({browsers: ['last 2 version']})
   ];
   return gulp.src(dir.src + '/*.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(
       postcss(
         processors
       )
     )
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(dir.dest));
 });
