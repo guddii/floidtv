fe192727ae37b3140ecb274de3eeea2b58c26f01
Florian Gudat <guddii@users.noreply.github.com>
2017-01-21 17:46:10 +0100

    Update middleware.js

--- infra/shared/middleware.js
+++ infra/shared/middleware.js
@@ -47,7 +47,10 @@ export default function (req, res, next) {
         template,
         JSON.parse(body),
         (err, html) => {
-          res.set({'Content-Type': 'text/html; charset=UTF-8'});
+          if (res.set) {
+            res.set({'Content-Type': 'text/html; charset=UTF-8'})
+          }
+
           res.end(html);
         }
       );
