3613565066e08496de45921e4beace64c99511b4
Florian Gudat <guddii@users.noreply.github.com>
2017-01-23 12:25:56 +0100

    Bugfix tabbar ie (#50)

--- app/components/tabbar/itemCtrl.js
+++ app/components/tabbar/itemCtrl.js
@@ -1,32 +1,40 @@
 function tabCtrl(id) {
-  document.querySelectorAll('[data-selector="tabbar/tab"]').forEach(function (element) {
-    if (element.dataset.id === id) {
-      element.classList.add('tabbar__tab__active');
-    } else {
-      element.classList.remove('tabbar__tab__active');
-    }
-  });
+
+	var element = document.querySelectorAll('[data-selector="tabbar/tab"]');
+	for (var i = 0; i < element.length; i++) {
+		if (element[i].dataset.id === id) {
+			element[i].classList.add('tabbar__tab__active');
+		} else {
+			element[i].classList.remove('tabbar__tab__active');
+		}
+	}
+
 }
 
 function tabItemCtrl(id) {
-  document.querySelectorAll('[data-selector="tabbar/item"]').forEach(function (element) {
-    if (element.dataset.id === id) {
-      element.classList.remove('tabbar__item--hidden');
-    } else {
-      element.classList.add('tabbar__item--hidden');
-    }
-  });
+
+	var element = document.querySelectorAll('[data-selector="tabbar/item"]');
+	for (var i = 0; i < element.length; i++) {
+		if (element[i].dataset.id === id) {
+			element[i].classList.remove('tabbar__item--hidden');
+		} else {
+			element[i].classList.add('tabbar__item--hidden');
+		}
+	}
+
 }
 
 function init() {
-  [...document.querySelectorAll('[data-selector="tabbar/tab"]')].forEach(function (tab) {
-    tab.addEventListener('click', function (event) {
-      event.preventDefault();
-      tabCtrl(this.dataset.id);
-      tabItemCtrl(this.dataset.id);
 
-    }, false);
-  })
+	var links = document.querySelectorAll('[data-selector="tabbar/tab"]');
+	for (var i = 0; i < links.length; i++) {
+		links[i].addEventListener('click', function (event) {
+			event.preventDefault();
+			tabCtrl(this.dataset.id);
+			tabItemCtrl(this.dataset.id);
+		}, false);
+	}
+
 }
 
 export default init;
